09489078cb269741de7d457a6a16c63a7286db91
devjoy <hiiamjm@gmail.com>
2017-07-20 22:02:13 +0530

    Update HelloWorld.java
    log message updated

--- src/com/test/HelloWorld.java
+++ src/com/test/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World - My First Project on GIT...");
+		System.out.println("Hello World - My First Project on GIT.....");
 
 	}
 

aabc111e3b6100c560efd4c084dfe9f10f899d14
Joydeep <hiiamjm@gmail.com>
2017-07-20 21:33:56 +0530

    Hello World Java Class Added

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.test;
+
+/**
+ * @author 596212
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World - My First Project on GIT...");
+
+	}
+
+}
